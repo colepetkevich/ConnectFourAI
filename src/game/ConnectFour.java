a0c971d3d05bbc3332052f407ec87d5488d5b629
colepetkevich <cpetkevich@gmail.com>
2019-11-05 14:19:19 -0800

    main menu added

--- src/game/ConnectFour.java
+++ src/game/ConnectFour.java
@@ -2,11 +2,14 @@ package game;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.image.BufferedImage;
 import java.util.Arrays;
 
 import javax.swing.JFrame;
 
 import engine.Button;
+import engine.Image;
+import engine.ImageFactory;
 import engine.Scene;
 import engine.Vector2;
 
@@ -23,7 +26,59 @@ public class ConnectFour extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-		newGameScene();	
+		//newGameScene();
+		newMainMenyScene();
+	}
+	
+	private static final Color MAIN_MENU_BACKGROUND_COLOR = new Color(246, 234, 123);
+	
+	private static final String LOGO_PATH = "res/Logo00.png";
+	private static final Vector2 LOGO_POSITION = new Vector2(0, 0.55f);
+	private static final float LOGO_HEIGHT = 0.875f;
+	
+	private static final Vector2 BUTTON_SIZE = new Vector2(2.0f, .2f);
+	private static final Vector2 FIRST_BUTTON_POSITION = new Vector2(0, -0.05f);
+	private static final float BUTTON_MARGIN = 0.05f;
+	private static final float BUTTON_FONT_SCALE = .85f;
+	private void newMainMenyScene()
+	{
+		createNewScene();
+		currentScene.setBackgroundColor(MAIN_MENU_BACKGROUND_COLOR);
+		
+		//adding logo
+		BufferedImage logoImage = ImageFactory.getImageFromPath(LOGO_PATH);
+		Image logo = new Image(currentScene);
+		logo.setImage(logoImage);
+		logo.setLocalSize(LOGO_HEIGHT * logoImage.getWidth() / logoImage.getHeight(), LOGO_HEIGHT);
+		logo.setLocalPosition(LOGO_POSITION);
+		
+		//adding buttons
+		Button easyButton = new Button(currentScene.CENTER, currentScene);
+		easyButton.setLocalSize(BUTTON_SIZE);
+		easyButton.setLocalPosition(FIRST_BUTTON_POSITION);
+		easyButton.setText("Easy");
+		easyButton.setFontScale(BUTTON_FONT_SCALE);
+		
+		Button mediumButton = new Button(currentScene.CENTER, currentScene);
+		mediumButton.setLocalSize(BUTTON_SIZE);
+		mediumButton.setLocalPosition(new Vector2(easyButton.getLocalPosition().x, easyButton.getLocalPosition().y - easyButton.getLocalSize().y - BUTTON_MARGIN));
+		mediumButton.setText("Medium");
+		mediumButton.setFontScale(BUTTON_FONT_SCALE);
+		
+		Button hardButton = new Button(currentScene.CENTER, currentScene);
+		hardButton.setLocalSize(BUTTON_SIZE);
+		hardButton.setLocalPosition(new Vector2(mediumButton.getLocalPosition().x, mediumButton.getLocalPosition().y - mediumButton.getLocalSize().y - BUTTON_MARGIN));
+		hardButton.setText("Hard");
+		hardButton.setFontScale(BUTTON_FONT_SCALE);
+		
+		Button twoPlayerButton = new Button(currentScene.CENTER, currentScene);
+		twoPlayerButton.setLocalSize(BUTTON_SIZE);
+		twoPlayerButton.setLocalPosition(new Vector2(hardButton.getLocalPosition().x, hardButton.getLocalPosition().y - hardButton.getLocalSize().y - BUTTON_MARGIN));
+		twoPlayerButton.setText("Two Player");
+		twoPlayerButton.setFontScale(BUTTON_FONT_SCALE);
+		twoPlayerButton.setMouseClickAction(() -> { newGameScene(); });
+		
+		currentScene.initialize(this);
 	}
 	
 	public static final int ROWS = 6;
@@ -32,6 +87,7 @@ public class ConnectFour extends JFrame
 	public static final char YELLOW = 'Y';
 	public static final char EMPTY = ' ';
 	
+	private static final Color GAME_BACKGROUND_COLOR = Color.DARK_GRAY;
 	private static final Vector2 CONNECT_FOUR_POSITION = new Vector2(0, 0);
 	private static final float CONNECT_FOUR_HEIGHT = 1.5f;
 	private static final Vector2 CONNECT_FOUR_SIZE = new Vector2(CONNECT_FOUR_HEIGHT * COLUMNS / ROWS, CONNECT_FOUR_HEIGHT);
@@ -39,13 +95,11 @@ public class ConnectFour extends JFrame
 	private char[] connectFour;
 	private char turn;
 	private Button[] inputButtons;
-	
 	private void newGameScene()
 	{
 		//creating a new scene
-		setScene(new Scene());
-		currentScene.setLayout(null);
-		currentScene.setBackgroundColor(Color.DARK_GRAY);		
+		createNewScene();
+		currentScene.setBackgroundColor(GAME_BACKGROUND_COLOR);		
 		
 		//creating board
 		connectFour = new char[ROWS * COLUMNS];
@@ -138,13 +192,18 @@ public class ConnectFour extends JFrame
 		resetButton.setText("Reset");
 		resetButton.setFontScale(.75f);
 		resetButton.setLocalSize(.5f, .2f);
-		resetButton.setLocalPosition(.30f, -.15f);
+		resetButton.setLocalPosition(.30f, -.4f);
 		
 		//setting resetButton command
-		resetButton.setMouseClickAction(() ->
-		{
-			newGameScene();
-		});
+		resetButton.setMouseClickAction(() -> { newGameScene(); });
+		
+		//creating backButton
+		Button backButton = new Button(currentScene.NORTH_WEST, currentScene);
+		backButton.setText("Back");
+		backButton.setFontScale(.75f);
+		backButton.setLocalSize(.5f, .2f);
+		backButton.setLocalPosition(.30f, -.15f);
+		backButton.setMouseClickAction(() -> { newMainMenyScene(); } );
 				
 		//initializing scene
 		currentScene.initialize(this);
@@ -185,7 +244,7 @@ public class ConnectFour extends JFrame
 			turn = RED;
 	}
 	
-	private void setScene(Scene scene)
+	private void createNewScene()
 	{
 		if (currentScene != null)
 		{
@@ -194,8 +253,9 @@ public class ConnectFour extends JFrame
 			remove(currentScene); 
 		}
 		
-		currentScene = scene;
+		currentScene = new Scene();
 		add(currentScene);
+		currentScene.setLayout(null);
 	}
 	
 	//main method
